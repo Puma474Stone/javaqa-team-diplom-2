1c2cd8b73cac554a201f37b03d39b424c5dce458
Николай <167803294+NikolayMartoplyas@users.noreply.github.com>
2024-07-27 19:31:06 +0300

    Update SavingAccountTest.java

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -44,10 +44,12 @@ public class SavingAccountTest {
 
     @Test
     public void shouldAddLessThanMaxBalance() {
-        SavingAccount account = new SavingAccount(
+       SavingAccount account = new SavingAccount(
                 2_000,
                 1_000,
-
+                10_000,
+                5
+        );
         account.add(3_000);
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
